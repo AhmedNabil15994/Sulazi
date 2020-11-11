79d78cb5e16ba99b29e368a88a882c8504b73e60
Ahmed Nabil <mody998866@gmail.com>
2020-11-11 09:12:31 +0200

    Fix

--- resources/views/Frontend/Layouts/master.blade.php
+++ resources/views/Frontend/Layouts/master.blade.php
@@ -6,7 +6,7 @@
         <meta charset="utf-8">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Sulazi - @yield('title')</title>
+        <title>{{ trans('main.website') }} - @yield('title')</title>
         @include('Frontend.Layouts.head')
         @include('Partials.notf_messages')
     </head>
